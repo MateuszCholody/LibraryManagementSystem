669f95397fde7becc1228166e1b8490e7730c14b
MateuszCholody <mateusz.cholody@gmail.com>
2021-05-23 18:51:05 +0200

    added login screen

---
+++ src/library/ui/login/login.java
@@ -0,0 +1,37 @@
+package library.ui.login;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import library.database.DatabaseHandler;
+
+public class login extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login_layout.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatabaseHandler.getInstance();
+            }
+        }).start();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
